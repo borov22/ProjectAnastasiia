32535fcf3c2fc63b1fdb72fa6caec9b7f733bec7
Nastya Tsaran <nastyatsaran@gmail.com>
2023-01-19 22:00:42 +0200

    commit

--- src/com/ua/tsaran/Main.java
+++ src/com/ua/tsaran/Main.java
@@ -2,6 +2,6 @@ package com.ua.tsaran;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
     }
 }
