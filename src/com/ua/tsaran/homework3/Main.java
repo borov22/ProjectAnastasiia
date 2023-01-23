7610e434e81b06ec5da05758e172e9ae15c8e661
Nastya Tsaran <nastyatsaran@gmail.com>
2023-01-23 22:08:32 +0200

    commit

---
+++ src/com/ua/tsaran/homework3/Main.java
@@ -0,0 +1,11 @@
+package com.ua.tsaran.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer x = 20, y = 5;
+            System.out.println(x+y);
+            System.out.println(x/y);
+            System.out.println(x-y);
+            System.out.println(x*y);
+    }
+}
