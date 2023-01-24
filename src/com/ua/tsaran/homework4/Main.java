6750f08817533528a5248c55581f7852ff51ecc2
Nastya Tsaran <nastyatsaran@gmail.com>
2023-01-24 21:46:47 +0200

    commit

---
+++ src/com/ua/tsaran/homework4/Main.java
@@ -0,0 +1,11 @@
+package com.ua.tsaran.homework4;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "Help me";
+            System.out.println(a.toUpperCase());
+            System.out.println(a.toLowerCase());
+            System.out.println(a.charAt(0));
+            System.out.println(a.charAt(a.length()-1));
+    }
+}
