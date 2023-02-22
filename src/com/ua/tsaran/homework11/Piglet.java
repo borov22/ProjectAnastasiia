b59e099a25db57ddfb5c70190cfde969823d9166
Nastya Tsaran <nastyatsaran@gmail.com>
2023-02-22 22:35:43 +0200

    HomeworkLesson12

---
+++ src/com/ua/tsaran/homework11/Piglet.java
@@ -0,0 +1,14 @@
+package com.ua.tsaran.homework11;
+
+public class Piglet extends Pig {
+    {
+        System.out.println("Initialisation block Piglet - 4");
+    }
+
+    {
+        System.out.println("Initialisation block Piglet - 5");
+    }
+    public Piglet() {
+        System.out.println("Constructor Piglet - 6");
+    }
+}
